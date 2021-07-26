d7c50e7190eb072e821e4a61d4e20d0154feccc3
Dayane <dayaneknlang@gmail.com>
2021-07-26 12:36:47 -0300

    Funcionalidade filtro por região finalizada

--- ecopatas/src/pages/points/PointsPage.js
+++ ecopatas/src/pages/points/PointsPage.js
@@ -1,50 +1,66 @@
-import React, { useEffect }from 'react'
+import React, { useEffect, useState } from 'react'
 import useRequestData from '../../hooks/useRequestData';
 import { BASE_URL } from '../../constants/urls';
 
-
 const PointsPage = () => {
-    
-    const point = useRequestData({}, `${BASE_URL}/pickupPoints.json`)
-    
-    
-    useEffect(() => {
-      inicialStateFilter()
-      
-    }, [])
 
-    const arrayPoints = []
+  const point = useRequestData({}, `${BASE_URL}/pickupPoints.json`)
 
-    for (let item of Object.values(point)) {
-      arrayPoints.push(item)
+  const [regionFilter, setRegionFilter] = useState("")
+
+  const onChange = (event) => {
+    setRegionFilter(event.target.value);
+  };
+
+  const arrayPoints = []
+
+  for (let item of Object.values(point)) {
+    arrayPoints.push(item)
+  }
+
+  const arrayFiltrado = arrayPoints.filter(item => {
+
+    item = item.region.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+    const filter = regionFilter.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+    if (item.includes(filter)) {
+
+      return true
     }
 
-    const pickupPointsList = arrayPoints.map((point) => {
-      return (
-        <div>      
-          <div key={point.id}>
-              <h2> {point.name} </h2>
-              <p> {point.cep} </p>
-              <p> {point.region} - {point.neighborhood} </p>
-              <p> {point.street}, {point.number} </p>
-              <p> Horário de Funcionamento: {point.schedule} </p>
-          </div>
+    return false
+  })
+
+  const pickupPointsList = arrayFiltrado.map((point) => {
+    return (
+      <div>
+        <div key={point.id}>
+          <h2> {point.name} </h2>
+          <p> {point.cep} </p>
+          <p> {point.region} - {point.neighborhood} </p>
+          <p> {point.street}, {point.number} </p>
+          <p> Horário de Funcionamento: {point.schedule} </p>
         </div>
-      )
-        
-    })
+      </div>
+    )
+
+  })
 
   return (
     <div>
-        Sou o PointsPage      
+      Sou o PointsPage
 
-        <div>
+      <div>Informe a região do Ponto de Arrecadação: </div>
+      <input onChange={onChange} />
 
-          {pickupPointsList.length > 0 ?
+      <div>
+
+        {pickupPointsList.length > 0 && regionFilter !== "" ?
           pickupPointsList : <h1>Carregando</h1>}
 
-        </div>
-        
+      </div>
+
     </div>
   )
 }
