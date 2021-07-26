e0bc0f6eb1ae6340aa99ee0e8ae1bccd72c6419d
Dayane <dayaneknlang@gmail.com>
2021-07-26 17:09:37 -0300

    Correção página points

--- ecopatas/src/components/Navbar.js
+++ ecopatas/src/components/Navbar.js
@@ -12,3 +12,12 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
+
+// position: absolute;
+// width: 375px;
+// height: 55px;
+// left: 0px;
+// top: 82px;
+
+// filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
